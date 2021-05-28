8bb691ee27bfdb90f76a2d7ca55b0d3019ccb8da
Tobin South <tobin.south@gmail.com>
2021-05-28 16:33:23 +0930

    Fix name/path issue

--- forecast_pipeline.sh
+++ forecast_pipeline.sh
@@ -6,7 +6,7 @@ NDAYS=$3 # Number of days after data date to forecast
 NSIMS=$4 # Total number of simulations to run
 
 jid1=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au sbatch_run_scripts/phoenix_run_estimator.sh ${DATADATE})
-jid2=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1 sbatch_run_scripts/run_posteriors.sh ${DATADATE})
+jid2=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1 sbatch_run_scripts/phoenix_run_posteriors.sh ${DATADATE})
 
 
 jid4=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2 sbatch_run_scripts/phoenix_all_states.sh ${NSIMS} ${NDAYS} ${DATADATE} ${STARTDATE})
