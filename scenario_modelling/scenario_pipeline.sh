8903f0bdf268138bbf0d1b9ac0cb5d0abb74fb0b
tobinsouth <tobin.south@gmail.com>
2021-07-27 22:11:32 +0930

    Fix scenario forecasting

--- scenario_modelling/scenario_pipeline.sh
+++ scenario_modelling/scenario_pipeline.sh
@@ -2,27 +2,17 @@
 
 DATADATE=$1 # Date of NNDSS data file
 NSIMS=$2 # Total number of simulations to run
+SCENARIO='no_reversion'
+SCENARIODATE='2021-07-05' # This doesn't matter for a no-reversion scenario
 
-# Assumes you've already run an EpyReff for the date.
-
+# Assumes you've already run an EpyReff for the date. If not, uncomment the following line.
+# jid_estimator=$(sbatch --parsable sbatch_run_scripts/phoenix_run_estimator.sh ${DATADATE})
 
 # We split the scenario params into the type and the date. It will apply the sec
-jid_posteriors_a=$(sbatch --parsable simulation_modelling/phoenix_scenario_R_L.sh ${DATADATE} "half_reversion" "2021-07-09")
-jid_posteriors_b=$(sbatch --parsable simulation_modelling/phoenix_scenario_R_L.sh ${DATADATE} "half_reversion" "2021-07-16")
+jid_posteriors_a=$(sbatch --parsable sbatch_run_scripts/phoenix_run_posteriors.sh ${DATADATE} ${SCENARIO})
 
-# Here the scenario parameter is just a filename extention. We can either run a single state for speed and later rename other parquets to include the scenario name for forecast plots or we can run all of the states.
-jid_simulate_a=$(sbatch --parsable --dependency=afterok:$jid_posteriors_a sbatch_run_scripts/phoenix_one_state.sh "NSW" ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-09")
+# Here the scenario parameter is just a filename extention.
+jid_simulate_a=$(sbatch --parsable --dependency=afterok:$jid_posteriors_a sbatch_run_scripts/phoenix_all_states.sh ${NSIMS} ${DATADATE} Delta "${SCENARIO}${SCENARIODATE}")
 
-jid_simulate_b=$(sbatch --parsable --dependency=afterok:$jid_posteriors_b sbatch_run_scripts/phoenix_one_state.sh "NSW" ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-16")
-
-# or
-
-jid_simulate_a=$(sbatch --parsable --dependency=afterok:$jid_posteriors_a sbatch_run_scripts/phoenix_all_states.sh ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-09")
-
-jid_simulate_b=$(sbatch --parsable --dependency=afterok:$jid_posteriors_b sbatch_run_scripts/phoenix_all_states.sh ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-16")
-
-
-# You need to make sure every state has a results/STATEDATE_sim_R_L_daysDelta[SCENARIO].parquet file. You cna just rename parquets for states that aren't relevant. collate states doesn't work with a single state.
-jid_savefigs_and_csv=$(sbatch --parsable --dependency=afterok:$jid_simulate_a sbatch_run_scripts/phoenix_final_plots_csv.sh ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-09")
-
-jid_savefigs_and_csv=$(sbatch --parsable --dependency=afterok:$jid_simulate_b sbatch_run_scripts/phoenix_final_plots_csv.sh ${NSIMS} ${DATADATE} Delta "half_reversion2021-07-16")
+# You need to make sure every state has a results/STATEDATE_sim_R_L_daysDelta[SCENARIO].parquet file. You can just rename parquets for states that aren't relevant. collate states doesn't work with a single state.
+jid_savefigs_and_csv=$(sbatch --parsable --dependency=afterok:$jid_simulate_a sbatch_run_scripts/phoenix_final_plots_csv.sh ${NSIMS} ${DATADATE} Delta "${SCENARIO}${SCENARIODATE}")
