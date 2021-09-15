dd21e4b4d51090b0b79d7d6ca9fe668d03a0c1aa
Dylan-J-Morris <dylan.morris7171@gmail.com>
2021-09-15 17:59:54 +0930

    changing more phoenix behaviour

--- sbatch_run_scripts/phoenix_all_states.sh
+++ sbatch_run_scripts/phoenix_all_states.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH -p batch
 #SBATCH --qos=express
-#SBATCH -N 8
+#SBATCH -N 1
 #SBATCH -n 12
 #SBATCH --time=1-00:00:00
 #SBATCH --mem=60GB
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=$USER@adelaide.edu.au
+#SBATCH --ntasks-per-core=1
 #SBATCH --array=0-7
 
 module load arch/haswell
