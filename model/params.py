a01bbe0484b5977926941be29f2f57831c4f6558
Dylan-J-Morris <dylan.morris7171@gmail.com>
2021-09-17 17:07:54 +0930

    Final fixes for the day.

--- model/params.py
+++ model/params.py
@@ -1,9 +1,9 @@
 ##### Key parameters
-on_phoenix = False   # flag for running on phoenix
+on_phoenix = True   # flag for running on phoenix
 run_inference = True    # whether the inference should be run
 run_inference_only = False
 use_linelist = True
-ncores = 4     # number of cores to use (this is relevant for the simulation)
+ncores = 12     # number of cores to use (this is relevant for the simulation)
 
 ##### Parameters for testing
 # use this to test the inference methods, fewer chains and samples => easy to debug and check summary output
