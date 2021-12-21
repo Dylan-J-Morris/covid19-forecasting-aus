f9bb71e302a86a682a9ffe69cb548881213ad21f
Dylan Morris <dylan.morris7171@gmail.com>
2021-12-22 09:56:08 +1030

    Adding separate driver script for non-Omicron states.

---
+++ sbatch_run_scripts/phoenix_all_states_non_omicron.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH --qos=express
+#SBATCH -N 1
+#SBATCH -n 12
+#SBATCH --time=12:00:00
+#SBATCH --mem=60GB
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=$USER@adelaide.edu.au
+#SBATCH --ntasks-per-core=1
+#SBATCH --array=0-6
+
+module load arch/haswell
+module load Python/3.6.1-foss-2016b
+source /hpcfs/users/$USER/local/virtualenvs/bin/activate
+
+states=("VIC" "SA" "QLD" "TAS" "WA" "ACT" "NT")
+
+NSIMS=$1
+DATADATE=$2
+SEEDING=$3
+
+python model/sim_model/run_state.py $NSIMS $DATADATE ${states[$SLURM_ARRAY_TASK_ID]} $SEEDING
+
+deactivate
