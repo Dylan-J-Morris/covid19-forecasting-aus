60438ec0cf61f9d640ebfadf00251d06b256401a
Dylan Morris <dylan.morris7171@gmail.com>
2022-01-14 01:55:06 +1030

    Simpler naming convention for running forecasts.

---
+++ generative_model/run_forecasts.jl
@@ -0,0 +1,25 @@
+"""
+This file is used to run the 
+"""
+
+include("simulate_states.jl")
+
+# parameters to pass to the main function 
+const file_date = ARGS[1]
+const nsims = parse(Int, ARGS[2])
+
+# states to simulate 
+const states_to_run = [
+    "NSW",
+    "QLD",
+    "TAS",
+    "VIC",
+    "WA",
+    "ACT",
+    "NT",
+]
+# const states_to_run = [
+#     "NSW",
+# ]
+# run main 
+simulate_all_states(file_date,states_to_run,nsims)
