7c0c34bee52bbebc90f8fb1052d68770ee5e6196
Dylan Morris <dylan.morris7171@gmail.com>
2022-01-13 20:31:51 +1030

    Declared inputs as const.

--- generative_model/run_forecasts_all_states.jl
+++ generative_model/run_forecasts_all_states.jl
@@ -5,7 +5,18 @@ This file is used to run the
 include("simulate_states.jl")
 
 # parameters to pass to the main function 
-file_date = ARGS[1]
-nsims = parse(Int, ARGS[2])
+const file_date = ARGS[1]
+const nsims = parse(Int, ARGS[2])
+
+# states to simulate 
+const states_to_run = [
+    "NSW",
+    "QLD",
+    "TAS",
+    "VIC",
+    "WA",
+    "ACT",
+    "NT",
+]
 # run main 
-simulate_all_states(file_date,nsims)
+simulate_all_states(file_date,states_to_run,nsims)
