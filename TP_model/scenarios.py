536a5b462051fd44bbf96291bd45e2991afeb04e
djmorris7 <dylan.morris7171@gmail.com>
2022-01-27 19:36:34 +1030

    Added scenarios for the return to school. Essentially a no-reversion with a multiplicative effect on the TP.

--- TP_model/scenarios.py
+++ TP_model/scenarios.py
@@ -1,47 +1,37 @@
 ##### TP forecasting parameters 
 # define scenarios for each state
-# scenarios = {
-#     'NSW': '',
-#     'QLD': '',
-#     'SA': '',
-#     'TAS': '',
-#     'VIC': '',
-#     'WA': '',
-#     'ACT': '',
-#     'NT': ''
-# }
-
-# # define scenario date for each state
-# scenario_dates = {
-#     'NSW': '',
-#     'QLD': '',
-#     'SA': '',
-#     'TAS': '',
-#     'VIC': '',
-#     'WA': '',
-#     'ACT': '',
-#     'NT': ''
-# }
-
 scenarios = {
-    'NSW': 'full_reversion',
-    'QLD': 'full_reversion',
-    'SA': 'full_reversion',
-    'TAS': 'full_reversion',
-    'VIC': 'full_reversion',
-    'WA': 'full_reversion',
-    'ACT': 'full_reversion',
-    'NT': 'full_reversion'
+    'NSW': 'school_opening_2022',
+    'QLD': 'school_opening_2022',
+    'SA': 'school_opening_2022',
+    'TAS': 'school_opening_2022',
+    'VIC': 'school_opening_2022',
+    'WA': 'school_opening_2022',
+    'ACT': 'school_opening_2022',
+    'NT': 'school_opening_2022',
 }
 
 # define scenario date for each state
-scenario_dates = {
-    'NSW': '2022-02-05',
-    'QLD': '2022-02-05',
-    'SA': '2022-02-05',
-    'TAS': '2022-02-05',
-    'VIC': '2022-02-05',
-    'WA': '2022-02-05',
-    'ACT': '2022-02-05',
-    'NT': '2022-02-05'
+scenario_dates_school_openings_2022 = {
+    'NSW': '2022-01-28',
+    'QLD': '2022-02-07',
+    'SA': '2022-02-02',
+    'TAS': '2022-02-09',
+    'VIC': '2022-01-28',
+    'WA': '2022-01-31',
+    'ACT': '2022-01-28',
+    'NT': '2022-01-31',
 }
+
+scenario_dates = scenario_dates_school_openings_2022
+# these dates are 
+# scenario_dates = {
+#     'NSW': '2022-01-28',
+#     'QLD': '2022-02-07',
+#     'SA': '2022-02-02',
+#     'TAS': '2022-02-09',
+#     'VIC': '2022-01-28',
+#     'WA': '2022-01-31',
+#     'ACT': '2022-01-28',
+#     'NT': '2022-01-31',
+# }
