f00e72269b3d8d47ab143be79f1a2aa39e2151c1
djmorris7 <dylan.morris7171@gmail.com>
2022-02-15 10:56:39 +1030

    Updated phoenix sbatch scripts.

--- sbatch_run_scripts/phoenix_plots.sh
+++ sbatch_run_scripts/phoenix_plots.sh
@@ -2,21 +2,18 @@
 #SBATCH -p batch
 #SBATCH --qos=express
 #SBATCH -N 1
-#SBATCH -n 12
-#SBATCH --time=12:00:00
-#SBATCH --mem=60GB
+#SBATCH -n 1
+#SBATCH --time=00:30:00
+#SBATCH --mem=20GB
+#SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=$USER@adelaide.edu.au
-#SBATCH --ntasks-per-core=1
-#SBATCH --array=0-7
 
 # module load arch/haswell (older versions of Julia here)
 module load arch/skylake
 # load latest Julia module
 module load Julia/1.7.0
 
-states=("NSW" "VIC" "SA" "QLD" "TAS" "WA" "ACT" "NT")
-
 DATADATE=$1
 
-julia -p 12 model/sim_model/run_state.py $DATADATE ${states[$SLURM_ARRAY_TASK_ID]}
+julia generative_model/run_forecast_plotting.jl $DATADATE
